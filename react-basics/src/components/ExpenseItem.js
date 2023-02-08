a700b7fb2cf7ff2bd1a26c437d5b6c105d74c877
anusha-c18 <anusha.chaturvedi@hpe.com>
2023-02-09 00:03:06 +0530

    cards

--- react-basics/src/components/ExpenseItem.js
+++ react-basics/src/components/ExpenseItem.js
@@ -1,24 +1,17 @@
 import "./ExpenseItem.css";
 import React from "react";
 import ExpenseDate from "./ExpenseDate";
-
+import Card from "./Card";
 function ExpenseItem(props) {
   return (
     // one root element only
-    // <div className="expense-item">
-    //   <ExpenseDate date={props.date}></ExpenseDate>
-    //   <p>{props.title}</p>
-    //   <div className="amount">
-    //     <p>{props.amount}</p>
-    //   </div>
-    // </div>
-    <div className="expense-item ">
+    <Card className="expense-item ">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description ">
         <h2>{props.title}</h2>
         <div className="expense-item__price ">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
