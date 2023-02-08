0702bb489646e51ce9afa1aff4635b502ff74e7b
anusha-c18 <anusha.chaturvedi@hpe.com>
2023-02-08 23:32:20 +0530

    nested components

--- react-basics/src/components/ExpenseItem.js
+++ react-basics/src/components/ExpenseItem.js
@@ -1,14 +1,22 @@
 import "./ExpenseItem.css";
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
   return (
     // one root element only
-    <div className="container">
-      <p>{props.date.toString()}</p>
-      <p>{props.title}</p>
-      <div className="amount">
-        <p>{props.amount}</p>
+    // <div className="expense-item">
+    //   <ExpenseDate date={props.date}></ExpenseDate>
+    //   <p>{props.title}</p>
+    //   <div className="amount">
+    //     <p>{props.amount}</p>
+    //   </div>
+    // </div>
+    <div className="expense-item ">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description ">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price ">${props.amount}</div>
       </div>
     </div>
   );
