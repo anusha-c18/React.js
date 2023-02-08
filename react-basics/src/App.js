572af2bd9a0cb1de6f08ff150ec3b1bd0279a7f7
anusha-c18 <anusha.chaturvedi@hpe.com>
2023-02-08 18:26:51 +0530

    first custom component

--- react-basics/src/App.js
+++ react-basics/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem'; 
 
 function App() {
   return (
     <div>
       <h2>hi</h2>
+      <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
 
 export default App;
+ 
