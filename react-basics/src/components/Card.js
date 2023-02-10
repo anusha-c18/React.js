ceaae2491a22e9a78890f2e8e0e93ea51b7aa20e
anusha-c18 <anusha.chaturvedi@hpe.com>
2023-02-10 14:46:58 +0530

    cards

--- react-basics/src/components/Card.js
+++ react-basics/src/components/Card.js
@@ -2,7 +2,11 @@ import "./Card.css";
 import React from "react";
 
 function Card(props) {
-  return <div className="card">{props.children}</div>;
+  const classes = "card " + props.className;
+  // this adds all the classes of the children that use the card custom element
+  return <div className={classes}>{props.children}</div>;
 }
+
+// props.children consists of the body of the card element used in the other files
 
 export default Card;
