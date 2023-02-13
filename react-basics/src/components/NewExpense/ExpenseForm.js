45b5b433efd220e04920f915056d6eff6e51df2c
anusha-c18 <anusha.chaturvedi@hpe.com>
2023-02-13 16:40:35 +0530

    using states that depend on previous states

--- react-basics/src/components/NewExpense/ExpenseForm.js
+++ react-basics/src/components/NewExpense/ExpenseForm.js
@@ -3,20 +3,25 @@ import React, { useState } from "react";
 
 function ExpenseForm() {
   const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setenteredAmount] = useState(0);
-  const [enteredDate, setenteredDate] = useState("2001-01-01");
+  const [enteredAmount, setEnteredAmount] = useState(0);
+  const [enteredDate, setEnteredDate] = useState("2001-01-01");
+
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    setEnteredAmount(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    console.log(event.target.value);
-    setEnteredTitle(event.target.value);
+    setEnteredDate(event.target.value);
   };
 
   return (
