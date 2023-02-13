439c30e5b7db46e9afa85dc020aa84c36d405758
anusha-c18 <anusha.chaturvedi@hpe.com>
2023-02-14 02:22:30 +0530

    removing button

--- react-basics/src/components/Expenses/ExpenseItem.js
+++ react-basics/src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,14 @@
 import "./ExpenseItem.css";
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
-  useState(props.title);
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Toothbrush");
-    console.log(title);
-  };
   return (
     // one root element only
     <Card className="expense-item ">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description ">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price ">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Submit</button>
